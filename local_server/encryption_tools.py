a2058ec972f5aa0e5d1ec07321208a3e8638335e
emilev <emiliano.levaro@gmail.com>
2024-08-13 22:40:37 +0200

    Started logic for encryption of folders but not there yet

--- local_server/encryption_tools.py
+++ local_server/encryption_tools.py
@@ -4,36 +4,27 @@ import json
 from base64 import b64encode, b64decode
 
 
-def encrypt(header, data):
+def encrypt(data, strong = False,  header = b""):
     # Generate key and cipher
-    key = get_random_bytes(16)
+    key = get_random_bytes(32 if strong else 16)
     cipher = AES.new(key, AES.MODE_GCM)  # Use Galois/Counter Mode
 
-    cipher.update(header)
+    if header:
+        cipher.update(header)
     cipher_text, tag = cipher.encrypt_and_digest(data)
 
     # Send key and parsed data
-    return key, [ b64encode(x).decode('utf-8') for x in (header, cipher_text, cipher.nonce, tag) ]
+    return b64encode(key).decode("utf-8"), [ b64encode(x).decode('utf-8') for x in (header, cipher_text, cipher.nonce, tag) ]
 
 
-def decryp(key, cipher_data):
+def decrypt(key, cipher_data):
     # Decode data and generate new cipher
     cipher_data = [ b64decode(e) for e in cipher_data]
 
-    cipher = AES.new(key, AES.MODE_GCM, nonce=cipher_data[2])
-    cipher.update(cipher_data[0])
+    cipher = AES.new(b64decode(key), AES.MODE_GCM, nonce=cipher_data[2])
+    if cipher_data[0]:
+        cipher.update(cipher_data[0])
 
     data = cipher.decrypt_and_verify(cipher_data[1], cipher_data[3])
     return data
 
-
-# testing funcs:
-
-header = b"header"
-data = b"info123"
-
-key, hidden_info = encrypt(header, data)
-print(key, hidden_info)
-
-found_data = decryp(key, hidden_info)
-print(found_data)
